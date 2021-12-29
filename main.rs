874c30b12b160573b6ec3de1cbd38e4b583ab1a0
ericschief <50526911+ericschief@users.noreply.github.com>
2021-12-29 11:44:47 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,161 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use rand::Rng;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead,BufReader, Error, ErrorKind};
+
+/*
+pub fn read1(path: &str,delimiter:char) -> Result<Vec<Vec<f64>>, Error>
+{
+    let file = File::open(path).expect("file not found");
+    let br = BufReader::new(file);
+    let mut v:Vec<Vec<f64>>  = vec![];
+    for line in br.lines()
+    {
+        let mut pair:Vec<f64> = vec![];
+
+        for x in line?.trim().split(delimiter)
+        {
+            let parsed:f64 = x.parse().unwrap();
+            pair.push(parsed);
+        }
+
+        v.push(pair);
+    }
+    Ok(v)
+}
+*/
+
+pub fn USPSlabels(file:&File)->Result<Vec<f64>,Error>
+{
+
+    let br = BufReader::new(file);
+    let n:usize = 7291;
+    let mut labels:Vec<f64> = vec![0.;n];
+    let mut count:usize = 0;
+    for line in br.lines()
+    {
+        //let temp = line.unwrap().chars().nth(0).unwrap().to_digit(10).unwrap() as f64 -1.;
+        let temp  = line?.trim().parse().unwrap();
+        labels[count] = temp;
+        count +=1;
+
+    }
+    Ok(labels)
+}
+pub fn USPSfeatures(file:&File) -> Result<Vec<Vec<f64>>,Error>
+{
+    let mut features:Vec<Vec<f64>> = vec![];
+    let br = BufReader::new(file);
+    for line in br.lines()
+    {
+        let mut row = vec![];
+        for x in line?.trim().split(',')
+        {
+            let parsed:f64 = x.parse().unwrap();
+            row.push(parsed);
+        }
+        features.push(row);
+    }
+
+    Ok(features)
+}
+
+
+pub fn norm(x:&Vec<f64>)->f64
+{
+    return x.iter().fold(0.,|sum,x|sum + x).sqrt()
+}
+
+pub fn affinityMatrix(x:&Vec<Vec<f64>>, sigma:f64)->Vec<Vec<f64>>
+{
+    let n = x.len();
+    //let m = x[0].len();
+    let mut w = vec![vec![0.;n]; n]; // allocate space for affinity matrix
+    let mut dif:Vec<f64>;
+    let mut val:f64;
+    for i in 0..n
+    {
+        for j in 0..n
+        {
+            dif = x[i].iter().zip(x[j].iter()).map(|(&x1,&x2)|x1-x2).collect();// compute difference of two rows
+            val = norm(&dif).powf(2.)/sigma; //find norm of vector
+            w[i][j] = (-val).exp();
+        }
+    }
+    return w
+}
+
+pub fn probtransMatrix(x:&Vec<Vec<f64>>)-> Vec<Vec<f64>>
+{
+    let n = x.len();
+    let m = x[0].len();
+    let mut p:Vec<Vec<f64>> = vec![vec![0.;m];n];
+    for i in 0..n
+    {
+        for j in 0..m
+        {
+            p[i][j] = x[i][j];// initialize matrix identical to x
+        }
+    }
+    for i in 0..n
+    {
+        let rowSum = x[i].iter().sum::<f64>();
+        for j in 0..m
+        {
+            p[i][j] /= rowSum; //sum row and divide each element in the row by that value
+        }
+    }
+    return p
+}
+
+
+/*
+
+//computes weighted graph of k-nearest neighbors for points in matrix
+pub fn kNN_graph(mat:&Vec<Vec<f64>>,k:usize) -> Vec<Vec<f64>>
+{
+    let n = mat.len();
+    let m = mat[0].len();
+    for i in 0..
+
+}
+*/
+//dynamic label propagation needs training data and test data to work on
+//sigma is a tuning parameter for learning
+pub fn DynamicLabelPropagation(trainFeatures:&Vec<Vec<f64>>,trainLabels:&Vec<f64>,num_samples:usize, sigma:f64)->Vec<Vec<f64>>
+{
+    let m = trainFeatures[0].len();
+    let mut trainFeatureSamples:Vec<Vec<f64>> = vec![vec![0.;m];num_samples];
+    let mut trainLabelSamples:Vec<f64> = vec![0.;num_samples];
+    let mut rng = rand::thread_rng();
+    for i in 0..num_samples
+    {
+        let randnum = rng.gen_range(0..trainFeatures.len());
+        trainLabelSamples[i] = trainLabels[randnum];
+        for j in 0..m
+        {
+            trainFeatureSamples[i][j] = trainFeatures[randnum][j];
+        }
+    }
+
+    let w = affinityMatrix(&trainFeatureSamples,sigma);
+    let _p_0 = probtransMatrix(&w);
+
+    return w
+}
+
+
+fn main()
+{
+
+    //load in training features and labels
+    let file1 = File::open("uspstrainlabels").unwrap();
+    let file2 = File::open("uspstrainfeatures").unwrap();
+    let _trainLabels = USPSlabels(&file1).unwrap();
+    let _trainFeatures = USPSfeatures(&file2).unwrap();
+
+
+
+}
