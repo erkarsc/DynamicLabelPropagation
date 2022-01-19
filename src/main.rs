389efea09e3e11a4f2169a1215c22f5b0e7a41da
Eric Schieferstein <erschieferstein@gmail.com>
2022-01-19 14:58:01 -0500

    altered code to use ndarray, performance increased dramatically

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,16 @@
 #![allow(non_snake_case)]
-#![allow(unused_imports)]
 
 use rand::Rng;
 use std::fs::File;
-use std::io;
 use std::default::Default;
-use std::io::{BufRead,BufReader, Error, ErrorKind};
+use std::io::{BufRead,BufReader, Error};
 use smartcore::algorithm::neighbour::cover_tree::CoverTree;
 use smartcore::math::distance::Distance;
+use ndarray::Array2;
+use ndarray::Data;
+use ndarray::prelude::*;
+use ndarray_stats::QuantileExt;
 type FloatMat = Vec<Vec<f64>>;
-
-pub trait Transpose
-{
-    fn T(&self) -> FloatMat;
-}
-
-impl Transpose for FloatMat
-{
-    fn T(self:&FloatMat) -> FloatMat
-    {
-        let r = self.len();
-        let c = self[0].len();
-        let mut transpose = vec![vec![0.;r];c];
-        for i in 0..c
-        {
-            for j in 0..r
-            {
-                transpose[i][j] = self[j][i];
-            }
-        }
-        return transpose
-    }
-}
-
-pub trait ArgMax
-{
-    fn argMax(&self)-> usize;
-}
-
-impl ArgMax for Vec<f64>
-{
-    fn argMax(self:&Vec<f64>) -> usize
-    {
-        let mut max = -100000.;
-        let mut ind:usize = 0;
-        for i in 0..self.len()
-        {
-            if self[i] > max
-            {
-                max = self[i];
-                ind = i;
-            }
-        }
-        return ind;
-    }
-}
 
 pub fn printMat(mat: &FloatMat)
 {
@@ -105,65 +61,6 @@ impl Default for Params
     }
 }
 
-pub fn scalarMult(c:f64,mut mat:FloatMat)->FloatMat
-{
-    for i in 0..mat.len()
-    {
-        for j in 0..mat[0].len()
-        {
-            mat[i][j]*=c;
-        }
-    }
-    return mat
-}
-
-pub fn matMult(mat1:&FloatMat, mat2:&FloatMat) -> FloatMat
-{
-    let r1 = mat1.len();
-    let r2 = mat2.len();
-    let c1 = mat1[0].len();
-    let c2 = mat2[0].len();
-    if c1 != r2
-    {
-        panic!("Mismatch of matrix dimensions");
-    }
-    let mut prod = vec![vec![0.;c2];r1];
-    for i in 0..r1
-    {
-        for j in 0..c2
-        {
-
-            for k in 0..r2
-            {
-              prod[i][j] += mat1[i][k] * mat2[k][j];
-            }
-        }
-    }
-    return prod
-}
-
-pub fn matSum(mat1:&FloatMat,mat2:&FloatMat)-> FloatMat
-{
-    let r1 = mat1.len();
-    let r2 = mat2.len();
-    let c1 = mat1[0].len();
-    let c2 = mat2[0].len();
-    if r1 != r2 || c1 != c2
-    {
-        panic!("Mismatch of matrix dimensions");
-    }
-    let mut sum = vec![vec![0.;c1];r1];
-    for i in 0..r1
-    {
-        for j in 0..c1
-        {
-            sum[i][j] = mat1[i][j] + mat2[i][j];
-        }
-    }
-    return sum
-}
-
-
 pub fn USPSlabels(file:&File)->Result<Vec<f64>,Error>
 {
     let br = BufReader::new(file);
@@ -182,7 +79,7 @@ pub fn USPSlabels(file:&File)->Result<Vec<f64>,Error>
 
 pub fn USPSfeatures(file:&File) -> Result<FloatMat,Error>
 {
-    let mut features:FloatMat = vec![];
+    let mut features = vec![];
     let br = BufReader::new(file);
     for line in br.lines()
     {
@@ -197,7 +94,9 @@ pub fn USPSfeatures(file:&File) -> Result<FloatMat,Error>
     Ok(features)
 }
 
-pub fn norm(x:&Vec<f64>)->f64
+pub fn norm<S>(x:&ArrayBase<S, Ix1>)->f64
+where
+    S:Data<Elem = f64>,
 {
     return x.iter().fold(0.,|sum,x|sum + x*x).sqrt();
 }
@@ -205,60 +104,58 @@ pub fn norm(x:&Vec<f64>)->f64
 #[derive(Clone)]
 struct DistanceStruct<'a>
 {
-    graph: &'a FloatMat,
+    graph: &'a Array2<f64>,
 }
 
 impl<'a> Distance<usize, f64> for DistanceStruct<'a>
 {
     fn distance(&self, a:& usize, b:& usize) -> f64
     {
-        self.graph[*a][*b]
+        self.graph[[*a,*b]]
     }
 }
 
-//creates affinity matrix of a given graph
-pub fn affinityMatrix(x:&FloatMat, params: &Params)->FloatMat
+pub fn dist_graph(mat:&Array2<f64>) -> Array2<f64>
 {
-    let n = x.len();
-    let mut w = vec![vec![0.;n]; n]; // allocate space for affinity matrix
-    let mut dif:Vec<f64>;
+    let n = mat.shape()[0];
+    let mut g = Array2::<f64>::zeros((n,n));//vec![vec![0.;n];n];
+    for i in 0..n
+    {
+        for j in 0..n
+        {
+            let temp = &mat.row(i)-&mat.row(j);//.iter().zip(mat[j].iter()).map(|(&mat1,&mat2)|mat1-mat2).collect();
+            g[[i,j]] = norm(&temp); // compute norm of difference between rows
+        }
+    }
+    return g
+}
+
+//creates affinity matrix of a given graph
+pub fn affinityMatrix(x:&Array2<f64>, params: &Params)->Array2<f64>
+{
+    let n = x.shape()[0];
+    let mut w = Array2::<f64>::zeros((n,n));//vec![vec![0.;n]; n]; // allocate space for affinity matrix
     let mut val:f64;
     for i in 0..n
     {
         for j in 0..n
         {
-            dif = x[i].iter().zip(x[j].iter()).map(|(&x1,&x2)|x1-x2).collect();// compute difference of two rows
+            let dif = &x.row(i)-&x.row(j);// compute difference of two rows
             val = norm(&dif).powf(2.)/params.sigma;
             //find norm of vector
-            w[i][j] = (-val).exp();
+            w[[i,j]] = (-val).exp();
 
         }
     }
     return w
 }
 
-
-pub fn dist_graph(mat:&FloatMat) -> FloatMat
-{
-    let n = mat.len();
-    let mut g:FloatMat = vec![vec![0.;n];n];
-    for i in 0..n
-    {
-        for j in 0..n
-        {
-            let temp = mat[i].iter().zip(mat[j].iter()).map(|(&mat1,&mat2)|mat1-mat2).collect();
-            g[i][j] = norm(&temp); // compute norm of difference between rows
-        }
-    }
-    return g
-}
-
 // calculates the similarity matrices that will be used to obtain the probabilistic transition matrices
-pub fn calcSimMatrix(sampleMat:&FloatMat, params:&Params) -> (FloatMat,FloatMat)
+pub fn calcSimMatrix(sampleMat:&Array2<f64>, params:&Params) -> (Array2<f64>,Array2<f64>)
 {
-    let num_samples = sampleMat.len();
+    let num_samples = sampleMat.shape()[0];
 
-    let mut ww = vec![vec![0.; num_samples]; num_samples];
+    let mut ww = Array2::<f64>::zeros((num_samples,num_samples));//vec![vec![0.; num_samples]; num_samples];
     let affMat = affinityMatrix(&sampleMat,params);
     let g = dist_graph(&sampleMat);
 
@@ -270,61 +167,61 @@ pub fn calcSimMatrix(sampleMat:&FloatMat, params:&Params) -> (FloatMat,FloatMat)
         let knn = tree.find(&i, params.k).unwrap();
         for tup in knn
         {
-            ww[i][*tup.2] = affMat[i][*tup.2];
+            ww[[i,*tup.2]] = affMat[[i,*tup.2]];
         }
     }
     return (ww, affMat)
 }
 
 //creates probabilistic transition matrices for W and 𝓦
-pub fn probTransMatrix(sampleMat:&FloatMat,params:&Params)-> (FloatMat,FloatMat)
+pub fn probTransMatrix(sampleMat:&Array2<f64>,params:&Params)-> (Array2<f64>,Array2<f64>)
 {
     let (w,ww) = calcSimMatrix(&sampleMat,&params);
-    let n = w.len();
-    let m = w[0].len();
-    let mut p_0:FloatMat = vec![vec![0.;m];n];
-    let mut ps:FloatMat = vec![vec![0.;m];n];
+    let n = w.shape()[0];
+    let m = w.len_of(Axis(0));
+    let mut p_0 = Array2::<f64>::zeros((n,m));//vec![vec![0.;m];n];
+    let mut ps = Array2::<f64>::zeros((n,m));//vec![vec![0.;m];n];
 
     for i in 0..n
     {
         for j in 0..m
         {
-            p_0[i][j] = w[i][j];// initialize matrix identical to x
-            ps[i][j] = ww[i][j];
+            p_0[[i,j]] = w[[i,j]];// initialize matrix identical to x
+            ps[[i,j]] = ww[[i,j]];
         }
     }
 
     for i in 0..n
     {
-        let rowSum1 = w[i].iter().sum::<f64>();
-        let rowSum2 = ww[i].iter().sum::<f64>();
+        let rowSum1:f64 = w.row(i).sum();
+        let rowSum2:f64 = ww.row(i).sum();
         for j in 0..m
         {
-            p_0[i][j] /= rowSum1; //sum row and divide each element in the row by that value
-            ps[i][j] /= rowSum2;
+            p_0[[i,j]] /= rowSum1; //sum row and divide each element in the row by that value
+            ps[[i,j]] /= rowSum2;
         }
     }
     return (p_0,ps)
 }
 //lambda matrix used in final iteration steps of algorithm
 //although lambda is a parameter, the algorithm is not very sensitive to changes in it
-pub fn lambMat(num_samples:usize, params:&Params) -> FloatMat
+pub fn lambMat(num_samples:usize, params:&Params) -> Array2<f64>
 {
     let n = 100;// This is the number of unlabeled samples
-    let mut mat = vec![vec![0.;num_samples+n];num_samples+n];
+    let mut mat = Array2::<f64>::zeros((num_samples+n,num_samples+n));//vec![vec![0.;num_samples+n];num_samples+n];
     for i in 0..num_samples
     {
-        mat[i][i] = params.lambda;
+        mat[[i,i]] = params.lambda;
     }
     return mat
 }
 
-pub fn labelMat(labeledFeatures:&FloatMat, labels:&Vec<f64>, unlabeledFeatures:&FloatMat,testLabels:&Vec<f64>,num_samples:usize) -> (FloatMat,FloatMat,Vec<usize>)
+pub fn labelMat(labeledFeatures:&FloatMat, labels:&Vec<f64>, unlabeledFeatures:&FloatMat,testLabels:&Vec<f64>,num_samples:usize) -> (Array2<f64>,Array2<f64>,Vec<usize>)
 {
     let m = labeledFeatures[0].len();
     let n = 100; // This is the number of unlabeled samples
-    let mut featureSamples = vec![vec![0.;m];num_samples+n];
-    let mut y:FloatMat = vec![vec![0.;10];num_samples+n];
+    let mut featureSamples = Array2::<f64>::zeros((num_samples+n,m));//vec![vec![0.;m];num_samples+n];
+    let mut y = Array2::<f64>::zeros((num_samples+n,10));//vec![vec![0.;10];num_samples+n];
     let mut rng = rand::thread_rng();
     let mut testLabelSamples:Vec<usize> = vec![];
     for i in 0..num_samples + n
@@ -332,10 +229,10 @@ pub fn labelMat(labeledFeatures:&FloatMat, labels:&Vec<f64>, unlabeledFeatures:&
         if i < num_samples
         {
             let randnum = rng.gen_range(0..labeledFeatures.len());
-            y[i][labels[randnum] as usize] = 1.;
+            y[[i,labels[randnum] as usize]] = 1.;
             for j in 0..m
             {
-                featureSamples[i][j] = labeledFeatures[randnum][j];
+                featureSamples[[i,j]] = labeledFeatures[randnum][j];
             }
         }
         else
@@ -344,7 +241,7 @@ pub fn labelMat(labeledFeatures:&FloatMat, labels:&Vec<f64>, unlabeledFeatures:&
             testLabelSamples.push(testLabels[randnum] as usize);
             for j in 0..m
             {
-                featureSamples[i][j] = unlabeledFeatures[randnum][j];
+                featureSamples[[i,j]] = unlabeledFeatures[randnum][j];
             }
         }
     }
@@ -353,56 +250,63 @@ pub fn labelMat(labeledFeatures:&FloatMat, labels:&Vec<f64>, unlabeledFeatures:&
 
 //dynamic label propagation needs training data and test data to work on
 //sigma is a tuning parameter
-pub fn dynamicLabelPropagation(labeledFeatures:&FloatMat,labels:&Vec<f64>,unlabeledFeatures:&FloatMat,testLabels:&Vec<f64>,num_samples:usize, params:&Params)->(FloatMat,Vec<usize>,Vec<usize>)
+pub fn dynamicLabelPropagation(labeledFeatures:&FloatMat,labels:&Vec<f64>,unlabeledFeatures:&FloatMat,testLabels:&Vec<f64>,num_samples:usize, params:&Params)->(Array2<f64>,Vec<usize>,Vec<usize>)
 {
     let(y,featureSamples,testLabelSamples) = labelMat(&labeledFeatures, &labels, &unlabeledFeatures,&testLabels, num_samples);
 
     let (mut p_0,ps) = probTransMatrix(&featureSamples,params);
     let lambdaMat = lambMat(num_samples, params);
-    let mut yNew:FloatMat = vec![];
+
+    let mut yNew = Array2::<f64>::zeros((p_0.shape()[0],y.shape()[1]));
+
+
 
     for _i in 0..params.max_iter
     {
-        yNew = matMult(&p_0,&y);
+        yNew = p_0.dot(&y);
+
         for i in 0..num_samples
         {
-            for j in 0..yNew[0].len()
+            for j in 0..yNew.shape()[1]
             {
-                yNew[i][j] = y[i][j];
+                yNew[[i,j]] = y[[i,j]];
             }
         }
-        //p_0 = ps.dot(&matSum(&p_0,&scalarMult(params.alpha,y.dot(&y.T())))).dot(&ps.T()).dot(&lambdaMat);
-        p_0 = matSum(&matMult(&matMult(&ps,&matSum(&p_0,&scalarMult(params.alpha,matMult(&y,&y.T())))),&ps.T()),&lambdaMat);
+        p_0 = &ps.dot(&(&p_0 + params.alpha*y.dot(&y.t()))).dot(&ps.t()) + &lambdaMat;
+        //p_0 = matSum(&matMult(&matMult(&ps,&matSum(&p_0,&scalarMult(params.alpha,matMult(&y,&y.t())))),&ps.t()),&lambdaMat);
     }
 
     let mut predictedLabels = vec![];
-    for i in num_samples..yNew.len()
+
+    for i in num_samples..yNew.shape()[0]
     {
-        predictedLabels.push(yNew[i].argMax());
+        predictedLabels.push(yNew.row(i).argmax().unwrap());
     }
+
     return (yNew,predictedLabels,testLabelSamples)
 }
 
 fn main()
 {
     //load in training features and labels
-    let file1 = File::open("TrainData/uspstrainlabels.txt").unwrap();
-    let file2 = File::open("TrainData/uspstrainfeatures.txt").unwrap();
-    let file3 = File::open("TestData/uspstestfeatures.txt").unwrap();
-    let file4 = File::open("TestData/uspstestlabels.txt").unwrap();
+    let file1 = File::open("./TrainData/uspstrainlabels.txt").unwrap();
+    let file2 = File::open("./TrainData/uspstrainfeatures.txt").unwrap();
+    let file3 = File::open("./TestData/uspstestfeatures.txt").unwrap();
+    let file4 = File::open("./TestData/uspstestlabels.txt").unwrap();
     let trainLabels = USPSlabels(&file1).unwrap();
     let trainFeatures = USPSfeatures(&file2).unwrap();
     let testLabels = USPSlabels(&file4).unwrap();
     let testFeatures = USPSfeatures(&file3).unwrap();
 
-    let test = dynamicLabelPropagation(&trainFeatures,&trainLabels,&testFeatures,&testLabels,100,&Default::default());
+    let test = dynamicLabelPropagation(&trainFeatures,&trainLabels,&testFeatures,&testLabels,200,&Default::default());
 
 
-    println!("{:?}", test.1);
-    println!("{:?}", test.2);
+    //println!("{:?}", test.1);
+    //println!("{:?}", test.2);
+
     let mut count:f64 = 0.;
 
-    for i in 0..test.1.len()
+    for i in 0..test.2.len()
     {
         if test.1[i] == test.2[i]
         {
